dd8a63f3e0a1937f30a2befa37677af8d4a57246
Sar2x <sar2x.18@gmail.com>
2020-04-15 10:46:05 -0400

    Added block meta data for burn book shelf

--- main/java/sar2x/codetiger/pmg/init/ModBlocks.java
+++ main/java/sar2x/codetiger/pmg/init/ModBlocks.java
@@ -6,8 +6,9 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import sar2x.codetiger.pmg.blocks.BlockBase;
+import sar2x.codetiger.pmg.blocks.burn_book_shelf;
 
 public class ModBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final Block BURN_BOOK_SHELF = new BlockBase("burn_book_shelf",Material.FIRE);
+	public static final Block BURN_BOOK_SHELF = new burn_book_shelf("burn_book_shelf",Material.FIRE);
 }
