1593441dd088836f2df2c6e5c369ebfab57427ff
Sar2x <sar2x.18@gmail.com>
2020-04-15 14:20:40 -0400

    Changed bookshelf drops

--- main/java/sar2x/codetiger/pmg/blocks/burn_book_shelf.java
+++ main/java/sar2x/codetiger/pmg/blocks/burn_book_shelf.java
@@ -1,7 +1,14 @@
 package sar2x.codetiger.pmg.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import sar2x.codetiger.pmg.init.ModItems;
+import sar2x.codetiger.pmg.items.burn_book;
 
 public class burn_book_shelf extends BlockBase {
 
@@ -15,5 +22,12 @@ public class burn_book_shelf extends BlockBase {
 		setLightLevel(1.0F);
 		// setLightOpacity()
 	}
-
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune){
+		return ModItems.BURN_BOOK;
+	}
+	@Override
+	public int quantityDropped(Random rand) {
+		return 3;
+	}
 }
